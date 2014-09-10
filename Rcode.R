b392aaae28cde18ec5db2a056e3989a6c8e9ad9d
Yet Nguyen <tienyettoan@gmail.com>
2014-09-10 15:21:49 -0500

    edit code

--- Rcode.R
+++ Rcode.R
@@ -1,38 +1,59 @@
-# Intracranial EEG (iEEG) data clips are organized in folders containing training and testing data for each human or canine subject. The training data is organized into ten minute EEG clips labeled "Preictal" for pre-seizure data segments, or "Interictal" for non-seizure data segments. Training data segments are numbered sequentially, while testing data are in random order. Within folders data segments are stored in .mat files as follow:
-#   
-#   preictal_segment_N.mat - the Nth preictal training data segment
-# interictal_segment_N.mat - the Nth non-seizure training data segment
-# test_segment_N.mat - the Nth testing data segment
-# Each .mat file contains a data structure with fields as follow:
-#   
-#   data: a matrix of EEG sample values arranged row x column as electrode x time.
-# data_length_sec: the time duration of each data row
-# sampling_frequency: the number of data samples representing 1 second of EEG data.
-# channels: a list of electrode names corresponding to the rows in the data field
-# sequence: the index of the data segment within the one hour series of clips.
-# For example, preictal_segment_6.mat has a sequence number of 6, 
-# and represents the iEEG data from 50 to 60 minutes into the preictal data.
-# Preictal training and testing data segments are provided covering one hour
-# prior to seizure with a five minute seizure horizon. 
-# (i.e. from 1:05 to 0:05 before seizure onset.) 
-# This pre-seizure horizon ensures that 1) seizures could be predicted with 
-# enough warning to allow administration of fast-acting medications, and 2) 
-#any seizure activity before the annotated onset that may have been 
-#missed by the epileptologist will not affect the outcome of the competition.
 library(R.matlab)
+library(plyr)
 
-path <- "/home/ntyet/Downloads/Dog_1"
-Dogid <- 1
-par(mfrow = c(3,3))
-for (i in 1:9){
-  segmentid <- paste("000", i, sep = "")
-  state <- "_interictal"
-  #state <- "_preictal"
-  # state <- "_test"
-  filename <- paste("Dog_", Dogid, state, "_segment_", segmentid, ".mat", sep = "")
-  pathname <- file.path(path, filename)
+path <- "/media/yet/DATA/seizure/Dog_"
+Dogid <- 5
+
+seq_data <- NULL
+state <- c("_interictal", "_preictal", "_test")
+
+#   str(data)
+#   str(data[[1]])
+#   str(data[[1]][[1]])# data matrix
+#   str(data[[1]][[2]]) # data.length.sec
+#   str(data[[1]][[3]])# sampling.frequency
+#   str(data[[1]][[4]])# chanels
+#   str(data[[1]][[5]])# sequence
+#   attr(data[[1]], "dimnames")
+#   attr(data, "header")
+i <- 1
+j <- 1
+pm1 <- proc.time()
+for(i in 1:50){
+  segmentid <- sprintf("%04d", i)
+  filename <- paste("Dog_", Dogid, state[j], "_segment_", segmentid, ".mat", sep = "")
+  pathname <- file.path(paste(path, Dogid, sep = ""), filename)
   data <- readMat(pathname)
-  
-  hist(data[[1]][[1]][1, ], nclass = 100, freq = T)
-  
+  seq_data[i] <- as.vector(data[[1]][[5]])
+    
 }
+proc.time() -pm1
+seq_data
+
+
+pm1 <- proc.time()
+seq_data <- laply(1:50, function(i){
+  segmentid <- sprintf("%04d", i)
+  filename <- paste("Dog_", Dogid, state[j], "_segment_", segmentid, ".mat", sep = "")
+  pathname <- file.path(paste(path, Dogid, sep = ""), filename)
+  data <- readMat(pathname)
+  return(as.vector(data[[1]][[5]]))
+}
+)
+proc.time() -pm1
+
+### extract matrix data ####
+
+pm1 <- proc.time()
+matrix_data <- llply(1:50, function(i){
+  segmentid <- sprintf("%04d", i)
+  filename <- paste("Dog_", Dogid, state[j], "_segment_", segmentid, ".mat", sep = "")
+  pathname <- file.path(paste(path, Dogid, sep = ""), filename)
+  data <- readMat(pathname)
+  return(summary(data[[1]][[1]]))
+}
+)
+proc.time() -pm1
+str(matrix_data)
+#gc()
+#gcinfo(FALSE)
