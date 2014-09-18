1ae8fe3bb7230da1efea1628d36c1c340cc8c3a0
unknown <ntyet@ts3-stat.stat.iastate.edu>
2014-09-18 00:30:58 -0500

    update code dog2 ar10 using arima

--- Seizure.R
+++ Seizure.R
@@ -36,83 +36,45 @@ get.seizure.file = function(dog.type,dog.count,seizure.type,file.count){
 # Dog2 has 16 channels
 
 # preictal ############
-J <- 42
-dog2_preictal_out <- data.frame(Date=as.Date(character()),
+n_segment <- c(42, 500, 1000)
+seizure <- c("preictal", "interictal", "test")
+type <- c("Dog", "Patient")
+i_type <- c(1:2)
+i_dog <- c(1,2,3,4,5)
+i_patient <- c(1,2)
+dog2_out <- data.frame(Date=as.Date(character()),
                  File=character(), 
                  User=character(), 
                  stringsAsFactors=FALSE) 
 pm1 <- proc.time()
-for(j in 1:J){
-  d <- get.seizure.file('Dog',2,'preictal',j)
+for(i_seizure in 1:3){ # i_seizure <- 1
+for(j in 1:n_segment[i_seizure]){
+  d <- get.seizure.file(type[1],i_dog[2],seizure[i_seizure],j) # j <- 1
   dat_mat <- d[[1]][[1]]
   n_channel <- nrow(dat_mat)
-  res <- ldply(1:n_channel, function(i){
-    quantile_out <- quantile(dat_mat[i,], probs = c(0, .25, .5, .75, 1))
-    sd_out <- sd(dat_mat[i,])
-    mean_out <- mean(dat_mat[i,])
-    pacf_out <- pacf(dat_mat[i,], plot = F)$acf[1:3]
+  res <- ldply(1:n_channel, function(i){ # i <- 1
+    p <- 10
+    des_out <- c(quantile(dat_mat[i,], probs = c(0, .25, .5, .75, 1)),
+                 sd(dat_mat[i,]),
+                 mean(dat_mat[i,]),
+                 pacf(dat_mat[i,], plot = F)$acf[1:p])
     
-    ar_out <- arma(dat_mat[i,], order = c(3,0))
-    summary_ar_out <- summary(ar_out)
-    coef_ar_out <-summary_ar_out$coef[,1]  # coef of AR(3)
-    sigma2_ar_out <- summary_ar_out$var
-    aic_ar_out <- summary_ar_out$aic
-    
-    
-    out <- c(dog = 2, seizure = "preictal", segment = j, channel = i, quantile_out, mean_out, sd_out, pacf_out, 
-             coef_ar_out, sigma2_ar_out, aic_ar_out)
+    ar_fit <- arima(dat_mat[i,], order = c(p,0,0))
+    ar_out <- c(ar_fit$coef, ar_fit$sigma2, ar_fit$aic)
+    out <- c(dog = 2, seizure = seizure[i_seizure], segment = j, channel = i, 
+             des_out, ar_out)
     names(out)  <- c("dog", "seizure", "segment", "channel",  "0_quantile","25_quantile", "50_quantile",
                      "75_quantile", "100_quantile", 
-                     "mean", "sd", "pacf_1", "pacf_2", "pacf_3",
-                     "ar3_ar1", "ar3_ar2", "ar3_ar3", "intercept",
+                     "mean", "sd", paste("pacf_", 1:p, sep = ""), 
+                     paste("ar", p,"_ar", 1:p, sep = ""), "intercept",  
                      "sigma2", "aic")
+    print(paste(type[1],i_dog[2],seizure[i_seizure],j,"channel", i,  sep = "_"))
     return(out)
     
   })
-dog2_preictal_out <- rbind(dog2_preictal_out, res)
-
+dog2_out <- rbind(dog2_out, res)
 }
-proc.time()-pm1
-write.csv(dog2_preictal_out, file = "dog2_preictal_ar3.csv", row.names = FALSE)
-
-
-
-# interictal segment#############
-J <- 2
-dog2_interictal_out <- data.frame(Date=as.Date(character()),
-                                File=character(), 
-                                User=character(), 
-                                stringsAsFactors=FALSE) 
-pm1 <- proc.time()
-for(j in 1:J){
-  d <- get.seizure.file('Dog',2,'interictal',j)
-  dat_mat <- d[[1]][[1]]
-  n_channel <- nrow(dat_mat)
-  res <- ldply(1:n_channel, function(i){
-    quantile_out <- quantile(dat_mat[i,], probs = c(0, .25, .5, .75, 1))
-    sd_out <- sd(dat_mat[i,])
-    mean_out <- mean(dat_mat[i,])
-    pacf_out <- pacf(dat_mat[i,], plot = F)$acf[1:3]
-    
-    ar_out <- arma(dat_mat[i,], order = c(3,0))
-    summary_ar_out <- summary(ar_out)
-    coef_ar_out <-summary_ar_out$coef[,1]  # coef of AR(3)
-    sigma2_ar_out <- summary_ar_out$var
-    aic_ar_out <- summary_ar_out$aic
-    
-    
-    out <- c(dog = 2, seizure = "interictal", segment = j, channel = i, quantile_out, mean_out, sd_out, pacf_out, 
-             coef_ar_out, sigma2_ar_out, aic_ar_out)
-    names(out)  <- c("dog", "seizure", "segment", "channel",  "0_quantile","25_quantile", "50_quantile",
-                     "75_quantile", "100_quantile", 
-                     "mean", "sd", "pacf_1", "pacf_2", "pacf_3",
-                     "ar3_ar1", "ar3_ar2", "ar3_ar3", "intercept",
-                     "sigma2", "aic")
-    return(out)
-    
-  })
-  dog2_interictal_out <- rbind(dog2_interictal_out, res)
-  
 }
+
 proc.time()-pm1
-write.csv(dog2_interictal_out, file = "dog2_interictal_ar3.csv", row.names = FALSE)
+write.csv(dog2_out, file = "dog2_ar10.csv", row.names = FALSE)
